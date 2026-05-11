165c799090d09d527e90aabdd77d4ec388c4a964
Terence Tao <tao@math.ucla.edu>
2026-05-11 19:40:19 -0400

    fixed all lean

--- Mathlib/Analysis/BoxIntegral/Stieltjes.lean
+++ Mathlib/Analysis/BoxIntegral/Stieltjes.lean
@@ -7,12 +7,15 @@ module
 
 public import Mathlib.Analysis.BoxIntegral.Basic
 public import Mathlib.Topology.EMetricSpace.BoundedVariation
+public import Mathlib.MeasureTheory.Integral.IntervalIntegral.Basic
+public import Mathlib.Analysis.Calculus.ContDiff.Defs
 
 namespace Stieltjes
 
 open BoxIntegral
 
-/-- If  `g : ℝ → M` is a map, `dg : BoxAdditiveMap Unit M ⊤` is the box additive map that sends a box `J` to `g(J.upper()) - g(J.lower())`.
+/-- If  `g : ℝ → M` is a map, `dg : BoxAdditiveMap Unit M ⊤` is the box additive map that sends
+a box `J` to `g(J.upper()) - g(J.lower())`.
 -/
 def deriv {M : Type*} [AddCommGroup M] (g : ℝ → M) :
     BoxAdditiveMap Unit M ⊤ :=
@@ -28,55 +31,75 @@ def deriv {M : Type*} [AddCommGroup M] (g : ℝ → M) :
 -/
 def interval (a b : ℝ) : Box Unit := sorry
 
-variable {E : Type*} {F : Type*} {G : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [NormedAddCommGroup F] [NormedSpace ℝ F] [NormedAddCommGroup G] [NormedSpace ℝ G]
+
+variable {E : Type*} {F : Type*} {G : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
+  [NormedAddCommGroup F] [NormedSpace ℝ F] [NormedAddCommGroup G] [NormedSpace ℝ G]
 variable (a b : ℝ) (B : E →L[ℝ] F →L[ℝ] G)
 
+/-- The scalar multiplication on the reals as a bilinear map. -/
+def smul_right : E →L[ℝ] ℝ →L[ℝ] E := sorry
+
+noncomputable def mul : ℝ →L[ℝ] ℝ →L[ℝ] ℝ := smul_right
+
+/-- A version of `LinearMap.flip` for continuous linear maps. -/
 def flip (B : E →L[ℝ] F →L[ℝ] G) : F →L[ℝ] E →L[ℝ] G := sorry
 
 /-- The Stieltjes integral of a function `f : ℝ → E` and `g : ℝ → F` given a bilinear
- map `B : E → F → G` and an interval `I` takes values in `G`.
--/
+ map `B : E → F → G` and an interval `I` takes values in `G`. -/
 def HasIntegral (f : ℝ → E) (g : ℝ → F) (L : G) : Prop :=
   BoxIntegral.HasIntegral (interval a b) IntegrationParams.Riemann (fun x ↦ f (x ()))
   (deriv (fun x ↦ (flip B) (g x))) L
 
 /-- Theorem A.1 of Montgomery Vaughan: if `f` is continuous and `g` is bounded variation
-then the Stieltjes integral exists.
--/
+then the Stieltjes integral exists. -/
 theorem exists_of_continuous_of_bounded_variation
   (f : ℝ → E) (g : ℝ → F)
   (hf : ContinuousOn f (Set.Icc a b)) (hg : BoundedVariationOn g (Set.Icc a b)) :
-  ∃ L, HasIntegral a b B f g L := by sorry
+  ∃ L, Stieltjes.HasIntegral a b B f g L := by sorry
 
 /-- Theorem A.2 of Montgomery Vaughan: if ∫_a^b f d g exists, then ∫_a^b g d f exists and
-∫_a^b g d f = g(b) * f(b) - g(a) * f(a) - ∫_a^b f d g.
--/
+∫_a^b g d f = g(b) * f(b) - g(a) * f(a) - ∫_a^b f d g. -/
 theorem integration_by_parts {f : ℝ → E} {g : ℝ → F} {L : G}
-  (hL : HasIntegral a b B f g L) :
-  HasIntegral a b (flip B) g f (B (g b) (f b) - B (g a) (f a) - L) := by sorry
+  (hL : Stieltjes.HasIntegral a b B f g L) :
+  Stieltjes.HasIntegral a b (flip B) g f (B (f b) (g b) - B (f a) (g a) - L) := by sorry
 
 /-- Theorem A.3 (a).  If g′ is continuous on [a, b], then
 Var[a,b]g = ∫_a^b |g'(x)|\ dx
 -/
-theorem variation_of_derivative {g : ℝ → F} (hgdiff : ContDiffOn ℝ g (Set.Icc a b)) :
-  eVariationOn g (Set.Icc a b) = ∫ x in a..b, ‖deriv g x‖ := by sorry
+theorem variation_of_derivative {g : ℝ → F} (hgdiff : ContDiffOn ℝ 1 g (Set.Icc a b)) :
+  (eVariationOn g (Set.Icc a b)).toReal = ∫ x in a..b, ‖_root_.deriv g x‖  := by sorry
 
-/- Task 6: Theorem A.3 (b).  If g′ is continuous on [a, b] and if in addition f is Riemann integrable, then
-∫_a^b f (x) dg(x) = ∫_a^b f (x)g′(x) dx. -/
+/-- Is there a better spelling for this? -/
+def RiemannIntegrable (f : ℝ → E) : Prop := BoxIntegral.Integrable (interval a b)
+  IntegrationParams.Riemann (fun x ↦ f (x ())) BoxAdditiveMap.volume
+
+/-- Theorem A.3 (b).  If g′ is continuous on [a, b] and if in addition f is
+Riemann integrable, then ∫_a^b f (x) dg(x) = ∫_a^b f (x)g′(x) dx. -/
 theorem integral_of_derivative {f : ℝ → E} {g : ℝ → F}
-  (hgdiff : ContDiffOn ℝ g (Set.Icc a b)) (hfint : BoxIntegral.Integrable (interval a b) IntegrationParams.Riemann f) :
-  HasIntegral a b B f g (∫ x in a..b, B (f x) (deriv g x)) := by sorry
+  (hgdiff : ContDiffOn ℝ 1 g (Set.Icc a b))
+  (hfint : RiemannIntegrable a b f) :
+  HasIntegral a b B f g (∫ x in a..b, B (f x) (_root_.deriv g x)) := by sorry
 
 
-/-- Task 7: Theorem A.4 Suppose that g has bounded variation, and put g^∗(x) = Var[a,x]g. Then
-
+/-- Theorem A.4. Suppose that g has bounded variation, and put g^∗(x) = Var[a,x]g. Then
 ∫_a^b f(x) dg(x) ≤ ∫_a^b | f (x)| dg∗(x).
-provided that both integrals exist.
--/
+provided that both integrals exist. -/
+theorem integral_le_integral_of_variation {f : ℝ → E} {g : ℝ → F}
+  (hgvar : BoundedVariationOn g (Set.Icc a b)) (L : G) (L' M : ℝ)
+  (hfgint : HasIntegral a b B f g L)
+  (hbound : ∀ e f, ‖B e f‖ ≤ M * ‖e‖ * ‖f‖)
+  (hfabs_gstar : HasIntegral a b mul (fun x ↦ ‖f x‖)
+    (fun x ↦ (eVariationOn g (Set.Icc a x)).toReal) L') :
+  ‖L‖ ≤ M * L' := by sorry
 
+/-- Relate sums ∑ f(n) with Stieltjes integrals ∫ f d ⌊x⌋ -/
+theorem sum_le_integral_nat_floor (f : ℝ → E) :
+  HasIntegral a b smul_right f (fun x ↦ Nat.floor x)
+  (∑ n ∈ Finset.Ico (Nat.ceil a) (Nat.ceil b), f n) := by sorry
 
-/-- Task 8: Relate sums ∑ f(n) with Stieltjes integrals ∫ f d ⌊x⌋ -/
-
+theorem sum_le_integral_floor (f : ℝ → E) :
+  HasIntegral a b smul_right f (fun x ↦ Int.floor x)
+  (∑ n ∈ Finset.Ico (Int.ceil a) (Int.ceil b), f n) := by sorry
 
 
 end Stieltjes
