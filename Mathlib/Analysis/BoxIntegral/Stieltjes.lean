bce6663e4948ed633995bcb45942228d273ba6b5
Terence Tao <tao@math.ucla.edu>
2026-05-11 16:28:13 -0400

    creating Stieltjes

---
+++ Mathlib/Analysis/BoxIntegral/Stieltjes.lean
@@ -0,0 +1,9 @@
+/-
+Copyright (c) ???. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: ???
+-/
+module
+
+public import Mathlib.Analysis.BoxIntegral.Basic
+
