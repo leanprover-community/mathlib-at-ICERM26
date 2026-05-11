97928707629db3e8e91b4eab6bf569de32b199fc
Terence Tao <tao@math.ucla.edu>
2026-05-11 17:03:47 -0400

    add tasks 5-6

--- Mathlib/Analysis/BoxIntegral/Stieltjes.lean
+++ Mathlib/Analysis/BoxIntegral/Stieltjes.lean
@@ -20,12 +20,16 @@ def StieltjesDerivative {M : Type*} [AddCommGroup M] (g : ℝ → M): BoxAdditiv
 /-- Task 3: Theorem A.1 of Montgomery Vaughan: if `f` is continuous and `g` is bounded variation then the Stieltjes integral exists.  -/
 
 
-
 /-- Task 4: Theorem A.2 of Montgomery Vaughan: if ∫_a^b f d g exists, then ∫_a^b g d f exists and
 ∫_a^b g d f = g(b) * f(b) - g(a) * f(a) - ∫_a^b f d g. -/
 
 
+/-- Task 5: Theorem A.3 (a).  If g′ is continuous on [a, b], then
+Var[a,b]g = ∫_a^b |g'(x)|\ dx
+-/
 
+/-- Task 6: Theorem A.3 (b).  If g′ is continuous on [a, b] and if in addition f is Riemann integrable, then
+∫_a^b f (x) dg(x) = ∫_a^b f (x)g′(x) dx. -/
 
 
 
