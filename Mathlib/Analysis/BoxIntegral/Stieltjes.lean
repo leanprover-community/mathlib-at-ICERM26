e669e087f7053008733c142a749ce72d0e55c190
Terence Tao <tao@math.ucla.edu>
2026-05-11 16:50:04 -0400

    add tasks 1-3

--- Mathlib/Analysis/BoxIntegral/Stieltjes.lean
+++ Mathlib/Analysis/BoxIntegral/Stieltjes.lean
@@ -9,8 +9,12 @@ public import Mathlib.Analysis.BoxIntegral.Basic
 
 namespace StieltjesIntegral
 
-/-- Turn a map `g : ℝ → M` to a BoxAdditiveMap `dg : BoxAdditiveMap Unit M I` -/
+/-- Task 1: Turn a map `g : ℝ → M` to a BoxAdditiveMap `dg : BoxAdditiveMap Unit M I` -/
 def StieltjesDerivative {M : Type*} [AddCommMonoid M] (I: WithTop (Box Unit)) (g : ℝ → M): BoxAdditiveMap Unit M I := by sorry
 
+/-- Task 2: Define a Stieltjes integral of a function `f : ℝ → E` and a function `g : R → (E →[L] F)` on an interval `I` by using `BoxIntegral`, the above additive map-/
+
+
+/-- Task 3: Theorem A.1 of Montgomery Vaughan: if `f` is continuous and `g` is bounded variation then the Stieltjes integral exists.  -/
 
 end StieltjesIntegral
