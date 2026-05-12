35993662c6b2017c56607d8bd097aaab0ae36501
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-05-12 23:04:12 +0200

    More basic API lemmas

--- Mathlib/JetsSection.lean
+++ Mathlib/JetsSection.lean
@@ -39,20 +39,29 @@ variable (Ψ s k) in
 def vanishesRelativeToOrder (γ : 𝕜 → B) : Prop :=
   ∀ i ≤ k, iteratedDeriv i (secToFun Ψ s ∘ γ) 0 = (0 : F)
 
+variable {γ γ' : 𝕜 → B}
+
 /- `vanishesRelativeToOrder` is independent of the choice of trivialisation -/
 lemma vanishesRelativeToOrder_change_triv
   {Ψ Ψ' : Trivialization F (TotalSpace.proj : TotalSpace F V → B)}
   [MemTrivializationAtlas Ψ] [MemTrivializationAtlas Ψ'] {γ : 𝕜 → B} :
   vanishesRelativeToOrder s k Ψ γ ↔ vanishesRelativeToOrder s k Ψ' γ := sorry
 
+/- `vanishesRelativeToOrder` only depends on the curve `γ` near `0` -/
+-- TODO bad name!
+lemma vanishesRelativeToOrder_congr_of_eventuallyEq_γ (heq : γ =ᶠ[𝓝 0] γ') :
+  vanishesRelativeToOrder s k Ψ γ ↔ vanishesRelativeToOrder s k Ψ γ' := sorry
 
--- only depends on the value of γ near 0
+-- XXX: is this actually true? the lemma below certainly is
+/- `vanishesRelativeToOrder` only depends on the section `s` near `x₀` -/
+lemma vanishesRelativeToOrder_congr_of_eventuallyEq (heq : (T% s) =ᶠ[𝓝 x₀] (T% t)) :
+  vanishesRelativeToOrder s k Ψ γ ↔ vanishesRelativeToOrder t k Ψ γ := sorry
 
--- is monotone in k
+/- `vanishesRelativeToOrder` is monotone in the order of vanishing -/
+lemma vanishesRelativeToOrder_mono {l : ℕ} (hs : vanishesRelativeToOrder s k Ψ γ) (hkl : l ≤ k) :
+   vanishesRelativeToOrder t l Ψ γ := sorry
 
--- natural map from k-jets to k-1-jets
-
-variable (F s k x₀) in
+variable (I F s k x₀) in
 /-- `s` vanishes to order `k` at `x₀` -/
 def vanishesToOrderAt : Prop :=
   ∀ (Ψ : Trivialization F (TotalSpace.proj : TotalSpace F V → B)),
@@ -60,9 +69,13 @@ def vanishesToOrderAt : Prop :=
   MemTrivializationAtlas Ψ → x₀ ∈ Ψ.baseSet → γ 0 = x₀ → CMDiffAt k γ 0 →
   vanishesRelativeToOrder s k Ψ γ
 
--- vanishesToOrderAt only depends on the value in a neighbourhood
+/- `vanishesToOrderAt` only depends on the section `s` near `x₀` -/
+lemma vanishesToOrderAt_congr_of_eventuallyEq (heq : (T% s) =ᶠ[𝓝 x₀] (T% t)) :
+  vanishesToOrderAt I F s k x₀ ↔ vanishesToOrderAt I F t k x₀ := sorry
 
--- is monotone in k
+/- `vanishesToOrderAt` is monotone in the order of vanishing -/
+lemma vanishesToOrderAt_mono {l : ℕ} (hs : vanishesToOrderAt I F s k x₀) (hkl : l ≤ k) :
+   vanishesToOrderAt I F s l x₀ := sorry
 
 variable (I)
 
@@ -86,9 +99,16 @@ def foo : Submodule 𝕜 (ContMDiffSection I F k V) where
   zero_mem' := sorry -- exercise!
   smul_mem' := sorry -- exercise!
 
-variable (V k x₀) in
+variable (V F k x₀) in
 /-- The module of `k`-jets of sections of `V` at `x₀` -/
 def kjets := (ContMDiffSection I F k V) ⧸ (foo I V k x₀)
+
+instance : AddCommMonoid (kjets I F V k x₀) := sorry
+
+instance : Module 𝕜 (kjets I F V k x₀) := sorry
+
+def kjets_mono : kjets I F V (k + 1) x₀ →ₗ[𝕜] kjets I F V k x₀ := sorry
+-- xxx: also from l to k? also continuous, when we put a topology?
 
 -- lemma: kjets does not
 
