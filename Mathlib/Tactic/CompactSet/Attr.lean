0213927acf9a2094c1427fbe4fd512f8da4bfdf8
Floris van Doorn <fpvdoorn@gmail.com>
2026-05-13 16:16:43 -0400

    WIP: compact_set and integrability
    Note: most of the work is in MathlibTest

---
+++ Mathlib/Tactic/CompactSet/Attr.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2026 Floris van Doorn. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Floris van Doorn
+-/
+
+module
+
+public import Lean.Meta.Tactic.Grind.RegisterCommand
+public import Mathlib.Init
+
+/-!
+Register grind attribute `compact_set`.
+-/
+register_grind_attr compact_set
---
+++ MathlibTest/fun_prop_integrable.lean
@@ -0,0 +1,157 @@
+import Mathlib
+import Mathlib.Tactic.CompactSet.Attr
+
+open MeasureTheory TopologicalSpace Set
+
+section Tac
+open Lean Parser Tactic
+/--
+A simple tactic that tries various lemmas to prove that a set is compact.
+The `simp` and explicitly marked lemmas are a temporary workaround to
+https://github.com/leanprover/lean4/issues/13725. -/
+macro "compact_set" config:optConfig : tactic => -- do
+  let attr : Ident := mkIdent `compact_set
+  `(tactic|(first | done | grind $config only [$attr:term, isCompact_Icc, Ici_inter_Iic]))
+
+end Tac
+/- maybe:
+IsCompact.of_isClosed_subset
+-/
+
+-- workaround to https://github.com/leanprover/lean4/issues/13725
+-- attribute [compact_set .] isCompact_Icc
+-- attribute [compact_set =] Ici_inter_Iic
+
+attribute [compact_set .]  IsCompact.union IsCompact.diff
+  IsCompact.image_of_continuousOn Continuous.continuousOn isCompact_closedBall isCompact_sphere
+attribute [compact_set =] closure_Icc
+attribute [compact_set .] IsCompact.inter_right IsCompact.inter_left IsCompact.inter
+  IsCompact.prod isCompact_univ isCompact_empty Set.Finite.isCompact isCompact_singleton
+attribute [compact_set .] isClosed_Icc isClosed_Ici isClosed_Iic isClosed_univ IsCompact.isClosed
+  IsCompact.closure
+
+lemma isClosed_uIcc {α : Type*} [TopologicalSpace α] [Lattice α] [OrderClosedTopology α] {a b : α} :
+  IsClosed (uIcc a b) := isClosed_Icc
+
+@[simp, compact_set =]
+lemma closure_uIcc {α : Type*} [TopologicalSpace α] [Lattice α] [OrderClosedTopology α] {a b : α} :
+  closure (uIcc a b) = uIcc a b := isClosed_uIcc.closure_eq
+
+@[compact_set .]
+lemma IsCompact_closure_uIoc
+    {α : Type*} [TopologicalSpace α] [LinearOrder α] [OrderTopology α] [DenselyOrdered α]
+    [CompactIccSpace α] {a b : α} : IsCompact (closure (uIoc a b)) := by
+  obtain rfl|h := eq_or_ne a b
+  · simp
+  · simp [h, isCompact_uIcc]
+
+@[compact_set .]
+lemma IsCompact_closure_Ioc
+    {α : Type*} [TopologicalSpace α] [LinearOrder α] [OrderTopology α] [DenselyOrdered α]
+    [CompactIccSpace α] {a b : α} : IsCompact (closure (Ioc a b)) := by
+  obtain rfl|h := eq_or_ne a b
+  · simp
+  · simp [h, isCompact_Icc]
+
+@[compact_set .]
+lemma IsCompact_closure_Ico
+    {α : Type*} [TopologicalSpace α] [LinearOrder α] [OrderTopology α] [DenselyOrdered α]
+    [CompactIccSpace α] {a b : α} : IsCompact (closure (Ico a b)) := by
+  obtain rfl|h := eq_or_ne a b
+  · simp
+  · simp [h, isCompact_Icc]
+
+@[compact_set .]
+lemma IsCompact_closure_Ioo
+    {α : Type*} [TopologicalSpace α] [LinearOrder α] [OrderTopology α] [DenselyOrdered α]
+    [CompactIccSpace α] {a b : α} : IsCompact (closure (Ioo a b)) := by
+  obtain rfl|h := eq_or_ne a b
+  · simp
+  · simp [h, isCompact_Icc]
+
+@[compact_set .]
+lemma IsCompact_closure_ball
+    {α : Type*} [PseudoMetricSpace α] [ProperSpace α] {x : α} {ε : ℝ} :
+    IsCompact (closure (Metric.ball x ε)) :=
+  Metric.isBounded_ball.isCompact_closure
+
+
+example : IsCompact <| Icc (1 : ℝ) (3 : ℝ) := by compact_set
+example : IsCompact <| closure (Ioo (1 : ℝ) (3 : ℝ)) := by compact_set
+example : IsCompact <| closure (Icc (1 : ℝ) (3 : ℝ)) := by compact_set
+example : closure (Icc (1 : ℝ) (3 : ℝ)) = Icc (1 : ℝ) (3 : ℝ) := by compact_set
+example : IsCompact <| closure (uIoc (1 : ℝ) (3 : ℝ)) := by compact_set
+example : IsCompact <| closure (Metric.ball (0 : Fin 5 → ℝ) 7) := by compact_set
+example : IsCompact <| closure (Metric.closedBall (0 : Fin 5 → ℝ) 7) := by compact_set
+example : IsCompact <| Metric.closedBall (0 : Fin 5 → ℝ) 7 := by compact_set
+example (x : EuclideanSpace ℝ (Fin 4)) : IsCompact <| closure (Metric.ball x 7) := by compact_set
+
+-- lemma Icc_inter_Ici {a b c : ℝ} : Icc a b ∩ Ici c = Icc (max a c) b := by exact? --ext; grind
+example {a b c : ℝ} : IsCompact (Icc a b ∩ Ici c) := by compact_set
+example {a b c d : ℝ} : IsCompact (Icc a b ∩ Icc c d) := by compact_set
+example {a b c d : ℝ} : IsCompact (closure (Icc a b ∩ Icc c d)) := by compact_set
+example {a b : ℝ} : IsCompact (closure (Ici a ∩ Iic b)) := by compact_set
+-- example {a b c d e : ℝ} : IsCompact {a, b, c, d, e, 0} := by compact_set
+
+
+
+attribute [fun_prop] LocallyIntegrableOn
+attribute [fun_prop] LocallyIntegrableOn.enorm ContinuousOn.locallyIntegrableOn
+
+attribute [fun_prop] LocallyIntegrable
+attribute [fun_prop] LocallyIntegrable.locallyIntegrableOn --Continuous.locallyIntegrable
+
+attribute [fun_prop] IntegrableOn
+
+attribute [fun_prop] IntervalIntegrable
+@[fun_prop] alias ⟨_, FunProp.intervalIntegrable_intro⟩ := intervalIntegrable_iff
+
+theorem FunProp.ContinuousOn.locallyIntegrableOn {X E : Type*} [MeasurableSpace X]
+    [TopologicalSpace X]
+    [NormedAddCommGroup E] {μ : Measure X} [OpensMeasurableSpace X] {K : Set X} {f : X → E} [IsLocallyFiniteMeasure μ]
+    [SecondCountableTopologyEither X E] (hf : ContinuousOn f K)
+    (hK : MeasurableSet K := by measurability) : LocallyIntegrableOn f K μ :=
+  _root_.ContinuousOn.locallyIntegrableOn hf hK
+
+@[fun_prop]
+theorem FunProp.LocallyIntegrableOn.integrableOn_isCompact {X ε : Type*} [MeasurableSpace X]
+    [TopologicalSpace X] [TopologicalSpace ε] [ContinuousENorm ε]
+    {f : X → ε} {μ : Measure X} {s : Set X}
+    [PseudoMetrizableSpace ε] (hf : LocallyIntegrableOn f (closure s) μ)
+    (hs : IsCompact (closure s) := by compact_set) :
+    IntegrableOn f s μ :=
+  (MeasureTheory.LocallyIntegrableOn.integrableOn_isCompact hf hs).mono_set subset_closure
+
+
+set_option trace.Meta.Tactic.fun_prop true
+example (f : ℝ → ℝ) (hf : Continuous f) : IntegrableOn f (Icc 1 3) := by
+  fun_prop (maxTransitionDepth := 3) (disch := first | compact_set | measurability)
+
+example (f : ℝ → ℝ) (hf : Continuous f) : IntervalIntegrable f volume 1 3 := by
+  fun_prop (maxTransitionDepth := 4) (disch := first | compact_set | measurability)
+
+example (f : ℝ → ℝ) (hf : Continuous f) : IntervalIntegrable (fun x ↦ f x) volume 1 3 := by
+  fun_prop (maxTransitionDepth := 4) (disch := first | compact_set | measurability)
+
+example (f g : ℝ → ℝ) (hf : Continuous f) (hf : Continuous g) :
+    IntervalIntegrable (f + g) volume 1 3 := by
+  fun_prop (maxTransitionDepth := 4) (disch := first | compact_set | measurability)
+
+example (f g : ℝ → ℝ) (hf : Continuous f) (hf : Continuous g) :
+    IntervalIntegrable (g ∘ f) volume 1 3 := by
+  fun_prop (maxTransitionDepth := 4) (disch := first | compact_set | measurability)
+
+example (f g : ℝ → ℝ) (hf : Continuous f) (hf : Continuous g) :
+    IntervalIntegrable (fun x ↦ f x + g x) volume 1 3 := by
+  fun_prop (maxTransitionDepth := 4) (disch := first | compact_set | measurability)
+
+example (f g : ℝ → ℝ) (hf : Continuous f) (hf : Continuous g) :
+    IntervalIntegrable (f * g) volume 1 3 := by
+  fun_prop (maxTransitionDepth := 4) (disch := first | compact_set | measurability)
+
+example (f g : ℝ → ℝ) (hf : Continuous f) (hf : Continuous g) :
+    IntervalIntegrable (fun x ↦ f x * g x) volume 1 3 := by
+  fun_prop (maxTransitionDepth := 4) (disch := first | compact_set | measurability)
+
+example (a b : ℝ) : IntegrableOn (fun x : ℝ => (Real.sin x : ℂ)) (Icc a b) := by
+  fun_prop (maxTransitionDepth := 4) (disch := first | compact_set | measurability)
