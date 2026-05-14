d65d114de2f8a9ce3f27024bc10f3945fa76c9d5
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-05-14 04:45:48 -0400

    feat: new definition of `bigO` and prove new theorems for it, including `bigO_add_bigO` as an equality

--- Mathlib/AsymptotcsTest.lean
+++ Mathlib/AsymptotcsTest.lean
@@ -2,51 +2,8 @@ module
 
 public import Mathlib
 
-open Real Topology
-open Filter
-
-namespace Asymptotics
-
-
-set_option linter.all false
-
-variable {α β γ δ E : Type*} [Norm E] (l : Filter α)
-
-def bigO (s : Set (α → E)) : Set (α → E) :=
-  { f | ∃ g ∈ s, f =O[l] g }
-
--- TODO: figure out the > 0 in this definition. It's not necessary, but
--- very helpful to have while proving things about bigO.
-def bigO' (s : Set (α → E)) : Set (α → E) :=
-  { f | ∃ g : Finset (α → E), (g : Set _) ⊆ s ∧ f =O[l] (fun x ↦ ⨆ i ∈ g, ‖i x‖) }
-
-@[simp, push]
-lemma mem_bigO' (f : α → E) (s : Set (α → E)) :
-    f ∈ bigO' l s ↔ ∃ c > 0, ∀ᶠ x in l, ∃ g ∈ s, ‖f x‖ ≤ c * ‖g x‖ := .rfl
-
-
-/-
-To spell it out more explicitly, I'm not sure the interpretation of O(O(s)) makes sense
-if we interpret it in terms of universal / existential quantifiers. In that setting,
-`O(O(s) + f x) ⊆ g x + O(x)`
-
-should be the same as
-
-`∀ f₁, f₁ = O(s) → ∀ f₂, f₂ = O(f₁ x + f x) → ∃ g₁, g₁ = O(x) ∧ f₂ x = g x + g₁ x`
-
-so `O(O(s) + f x)` consists of `f₂` such that `∀ f₁ ∈ O(s), f₂ ∈ O(f₁ x + f x)`
-
-If we reverse the equation, we would get
-
-`∀ g₁, g₁ = O(x) → ∃ f₁, f₁ = O(s) ∧ ∃ f₂, f₂ = O(f₁ x + f x) ∧ f₂ x = g x + g₁ x`
-
-so `O(O(s) + f x)` consists of `f₂` such that `∃ f₁ ∈ O(s), f₂ ∈ O(f₁ x + f x)`
-
-- Arend
--/
-
-/-
-
+/-!
+# Big O and little o notation
 Additional things we need to support:
 
 - Other relations rather than just =, for example ≤, =ᶠ[l], ≪. That is,
@@ -57,54 +14,97 @@ Additional things we need to support:
 
 - Constants that are allowed to depend on some specific subset of the variables, i.e.
   $O_{A, ε}(f(x))$
-
 -/
 
-def RightSerial (r : α → β → Prop) (s₁ : Set α) (s₂ : Set β) : Prop :=
-  ∀ x₁ ∈ s₁, ∃ x₂ ∈ s₂, r x₁ x₂
+open Real Topology
+open Filter
 
-notation3 x " RS[" r "] " y => RightSerial r x y
+namespace Asymptotics
 
-universe u v w
+attribute [refl] isBigO_refl
 
-instance (r : α → β → Prop) (s : β → γ → Prop) (t : α → γ → Prop) [Trans r s t] :
-  Trans (RightSerial r) (RightSerial s) (RightSerial t) where
-  trans hab hbc := by
-    simp only [RightSerial] at hab hbc ⊢
-    have := @Trans.trans (r := r) (s := s) (t := t) _ _ _ _
-    grind
+set_option linter.all false
 
-@[simp]
-lemma rightSerial_eq (a b : Set α) : (a RS[Eq] b) ↔ a ⊆ b := by
-  unfold RightSerial
-  grind
+variable {α β γ δ E : Type*} [SeminormedAddCommGroup E] (l : Filter α)
 
-@[simp, gcongr]
-lemma RightSerial.singleton_RS_singleton {r : α → α → Prop} (a : α) (b : α) :
-    ({a} RS[r] {b}) ↔ r a b := by
-  unfold RightSerial
-  simp
+def bigO (s : Set (α → E)) : Set (α → E) :=
+  { f | ∃ (ι : Type) (_ : Fintype ι) (g : ι → (α → E)),
+      (∀ i, g i ∈ s) ∧ f =O[l] fun j ↦ ∑ i, ‖g i j‖ }
 
-@[simp, push]
-lemma mem_bigO (f : α → E) (s : Set (α → E)) : f ∈ bigO l s ↔ ∃ g ∈ s, f =O[l] g := .rfl
+attribute [push] Classical.skolem
+
+lemma mem_bigO (f : α → E) (s : Set (α → E)) :
+    f ∈ bigO l s ↔ ∃ (ι : Type) (_ : Fintype ι) (g : ι → (α → E)),
+      (∀ i, g i ∈ s) ∧ f =O[l] fun j ↦ ∑ i, ‖g i j‖ :=
+  .rfl
 
 @[gcongr]
-lemma bigO_mono_set (s₁ s₂ : Set (α → E)) (h : s₁ ⊆ s₂) : bigO l s₁ ⊆ bigO l s₂ := by
-  intro x
-  simp only [mem_bigO, forall_exists_index, and_imp]
-  grind
+lemma bigO_subset_bigO (s₁ s₂ : Set (α → E)) (h : s₁ ⊆ s₂) : bigO l s₁ ⊆ bigO l s₂ := by
+  unfold bigO
+  gcongr
 
-/- Written by Claude -/
+@[gcongr]
+lemma mem_bigO_mono (s : Set (α → E)) {f g : α → E} (h : f =O[l] g) :
+    g ∈ bigO l s → f ∈ bigO l s := by
+  simp only [mem_bigO]
+  gcongr
+  exact h.trans
+
+lemma mem_bigO_empty {f : α → E} : f ∈ bigO l ∅ ↔ f =O[l] fun x ↦ (0 : ℝ) := by
+  rw [mem_bigO]
+  constructor
+  · rintro ⟨ι, instι, z, hz, hf⟩
+    simp at hz
+    have (i : ι) : z i = 0 := by exfalso; exact hz i
+    simpa [this] using hf
+  · intro h
+    use Empty, inferInstance, fun _ _ ↦ 0, nofun
+    simpa
+
+lemma subset_bigO (s : Set (α → E)) : s ⊆ bigO l s := by
+  intro f hf
+  rw [mem_bigO]
+  use Unit, inferInstance
+  use fun _ ↦ f, fun _ ↦ hf
+  simp
+  rfl
+
+@[simp]
+lemma mem_bigO_singleton (f g : α → E) :
+    f ∈ bigO l {g} ↔ f =O[l] g := by
+  constructor
+  · rw [mem_bigO]
+    rintro ⟨ι, instι, z, hz, hf⟩
+    refine hf.trans ?_
+    apply IsBigO.sum
+    rintro i -
+    apply IsBigO.norm_left
+    simp at hz
+    rw [hz]
+  · intro h
+    grw [h, ← subset_bigO]
+    exact Set.mem_singleton _
+
 @[simp]
 lemma bigO_bigO (s : Set (α → ℝ)) : bigO l (bigO l s) = bigO l s := by
   ext f
-  simp only [mem_bigO]
-  refine ⟨?_, ?_⟩
-  · rintro ⟨g, ⟨h, hh, hgh⟩, hfg⟩
-    exact ⟨h, hh, IsBigO.trans hfg hgh⟩
-  · rintro ⟨g, hg, hfg⟩
-    exact ⟨g, ⟨g, hg, isBigO_refl _ _⟩, hfg⟩
-
+  constructor
+  · rw [mem_bigO]
+    rintro ⟨ι, instι, z, hz, hf⟩
+    simp_rw [mem_bigO] at hz
+    push ∀ _, _ at hz
+    obtain ⟨ι', instι', z', hz', hz⟩ := hz
+    rw [mem_bigO]
+    use (i : ι) × ι' i, inferInstance
+    use fun i ↦ z' i.1 i.2, fun i ↦ hz' i.1 i.2
+    grw [hf]
+    simp_rw [Fintype.sum_sigma]
+    convert IsBigO.sum_congr ?_ using 3
+    · symm; exact abs_of_nonneg (by positivity)
+    rintro i -
+    exact IsBigO.abs_left (hz i)
+  · gcongr
+    apply subset_bigO
 
 def map (s₁ : Set (α → β → γ)) (s₂ : Set (α → β)) : Set (α → γ) :=
   { g | ∃ f₁ ∈ s₁, ∃ f₂ ∈ s₂, g = fun x ↦ f₁ x (f₂ x) }
@@ -137,35 +137,6 @@ lemma map_iUnion_right {ι : Sort*} (s : Set (α → β → γ)) (t : ι → Set
   simp only [map, Set.mem_setOf_eq, Set.mem_iUnion]
   grind
 
-/- Written by Claude -/
-@[gcongr]
-lemma RSerial.iUnion_RS_iUnion {ι : Sort*} {r : α → β → Prop} {s : ι → Set α} {t : ι → Set β}
-    (h : ∀ i, s i RS[r] t i) : (⋃ i, s i) RS[r] (⋃ i, t i) := by
-  rintro x ⟨_, ⟨i, rfl⟩, hx⟩
-  obtain ⟨y, hy, hxy⟩ := h i x hx
-  exact ⟨y, Set.mem_iUnion.mpr ⟨i, hy⟩, hxy⟩
-
-class MapClass {α β γ : Type*} (r : (α → γ) → (α → γ) → Prop)
-    (r₁ : outParam ((α → β → γ) → (α → β → γ) → Prop)) (r₂ : outParam ((α → β) → (α → β) → Prop)) where
-  imp {f₁ f₂ f₁' f₂'} : r₁ f₁ f₁' → r₂ f₂ f₂' → r (fun x ↦ f₁ x (f₂ x)) (fun x ↦ f₁' x (f₂' x))
-
-@[gcongr]
-lemma map_rightSerial_map {s₁ s₁' : Set (α → β → γ)} {s₂ s₂' : Set (α → β)} {r r₁ r₂} [MapClass r r₁ r₂]
-    (h₁ : s₁ RS[r₁] s₁') (h₂ : s₂ RS[r₂] s₂') : map s₁ s₂ RS[r] map s₁' s₂' := by
-  rintro g ⟨f₁, hf₁, f₂, hf₂, rfl⟩
-  obtain ⟨f₁', hf₁', hff₁⟩ := h₁ f₁ hf₁
-  obtain ⟨f₂', hf₂', hff₂⟩ := h₂ f₂ hf₂
-  refine ⟨fun x ↦ f₁' x (f₂' x), ⟨f₁', hf₁', f₂', hf₂', rfl⟩, ?_⟩
-  exact MapClass.imp hff₁ hff₂
-
-instance {l : Filter α} : MapClass (α := α) (β := β) (γ := γ) (EventuallyEq l) (EventuallyEq l) (EventuallyEq l) where
-  imp h₁ h₂ := by
-    filter_upwards [h₁, h₂] with x hx₁ hx₂
-    simp [hx₁, hx₂]
-
-instance : MapClass (α := α) (β := β) (γ := γ) Eq Eq Eq where
-  imp h₁ h₂ := by simp [h₁, h₂]
-
 @[simp]
 lemma mem_pure (x : α → β) (y : β) : x ∈ pure y ↔ x = fun _ ↦ y := by
   simp [pure]
@@ -173,22 +144,22 @@ lemma mem_pure (x : α → β) (y : β) : x ∈ pure y ↔ x = fun _ ↦ y := by
 -- Question: Is this meaningful if we replace {fun x ↦ x} with a different `Set (ℝ → ℝ)`?
 -- exp x = 1 + O[𝓝 0](x)
 lemma exp_at_one : map (pure exp) {fun x ↦ x} ⊆ map (pure <| HAdd.hAdd 1) (bigO (𝓝 0) {fun x ↦ x}) := by
-  have := Real.exp_sub_sum_range_isBigO_pow 1
-  simp at this
   intro y
   simp
   rintro rfl
   use fun x ↦ Real.exp x - 1
-  refine ⟨this, by ring_nf⟩
+  constructor
+  · simpa using Real.exp_sub_sum_range_isBigO_pow 1
+  · ring_nf
 
 lemma exp_at_one_ : map (pure exp) {fun x ↦ x} ⊆ map (map (pure HAdd.hAdd) (pure 1)) (bigO (𝓝 0) {fun x ↦ x}) := by
-  have := Real.exp_sub_sum_range_isBigO_pow 1
-  simp at this
   intro y
   simp
   rintro rfl
   use fun x ↦ Real.exp x - 1
-  refine ⟨this, by ring_nf⟩
+  constructor
+  · simpa using Real.exp_sub_sum_range_isBigO_pow 1
+  · ring_nf
 
 lemma exp_at_one' {l : Filter ℝ} {f : ℝ → ℝ} (hf : Filter.Tendsto f l (𝓝 0)) :
     map (pure exp) {f} ⊆ map (pure <| HAdd.hAdd 1) (bigO l {f}) := by
@@ -256,7 +227,7 @@ lemma exp_at_one_set {l : Filter ℝ} {s : Set (ℝ → ℝ)}
   push map
   simp only [map_iUnion_right, Set.iUnion_subset_iff]
   rintro f hf
-  grw [exp_at_one'' (hs _ hf), bigO_mono_set]
+  grw [exp_at_one'' (hs _ hf), bigO_subset_bigO]
   simp [hf]
 
 -- O[l](f x) + O[l](f x) = O[l](f x)
@@ -273,40 +244,6 @@ lemma bigO_add_bigO_self (f : α → ℝ) : map (map (pure HAdd.hAdd) (bigO l {f
     simp only [add_zero, and_true]
     exact isBigO_zero f l
 
-lemma bigO_add_bigO_set_eq_union (s₁  s₂ : Set (α → ℝ)) : map (map (pure HAdd.hAdd) (bigO' l s₁)) (bigO' l s₂) ⊆ bigO' l (s₁ ∪ s₂) := by
-  intro y
-  push _ ∈ _
-  simp
-  rintro f₀ c₀ hc₀ hf₀ f₁ c₁ hc₁ hf₁ rfl
-  use c₀ + c₁, (by linarith)
-  filter_upwards [hf₀, hf₁]
-  intro x ⟨g₀, hg₀s, hg₀⟩ ⟨g₁, hg₁s, hg₁⟩
-  grw [abs_add_le, hg₀, hg₁]
-  simp_rw [add_mul]
-  by_cases! h : |g₀ x| ≤ |g₁ x|
-  · grw [h]
-    use g₁, (.inr hg₁s)
-  · grw [h]
-    use g₀, (.inl hg₀s)
-
--- O[l](f x) + O[l](f x) = O[l](f x)
-lemma bigO_add_bigO_set (f : Set (α → ℝ)) : map (map (pure HAdd.hAdd) (bigO' l f)) (bigO' l f) ⊆ bigO' l f := by
-  grw [bigO_add_bigO_set_eq_union, Set.union_self]
-
--- Do we even need this? It's just a grw?
-lemma bigO_add_bigO_eq_left (s₁ s₂ : Set (α → ℝ)) (h : bigO' l s₂ ⊆ bigO' l s₁) : map (map (pure HAdd.hAdd) (bigO' l s₁)) (bigO' l s₂) ⊆ bigO' l s₁ := by
-  grw [h, bigO_add_bigO_set]
-
-set_option autoImplicit false
-/-
-  (n+1)^(e^(1/n))
-  = (n+1)^(1 + O(1/n)) := _
-  = exp ( (log n + O(1/n)) * (1 + O(1/n)) ) := _
-  = exp (log n + O(log n/n)) := _
-  = n * (1 + O(log n/n)) := _
-  = n + O(log n) := _
--/
-
 lemma map_eq (s₁ : Set (α → β → γ)) (s₂ : Set (α → β)) :
     map s₁ s₂ = ⋃ i₁ ∈ s₁, ⋃ i₂ ∈ s₂, {fun x ↦ i₁ x (i₂ x)} := by
   ext x
@@ -315,6 +252,172 @@ lemma map_eq (s₁ : Set (α → β → γ)) (s₂ : Set (α → β)) :
 lemma singleton_eq_map_singleton_singleton (f : α → β → γ) (a : α → β) :
     ({fun x ↦ f x (a x)} : Set (α → γ)) = map {f} {a} := by
   simp [map_eq, pure]
+
+macro "magic_tac" loc:(Lean.Parser.Tactic.location)? : tactic => `(tactic|
+  simp only [map_eq, mem_pure, Set.mem_singleton_iff, Set.iUnion_iUnion_eq_left,
+    Set.mem_iUnion, exists_prop, Set.iUnion_exists, Set.biUnion_and'] $[$loc]?)
+
+lemma mul_bigO {s₁ s₂ : Set (α → ℝ)} :
+    map (map (pure HMul.hMul) s₁) (bigO l s₂) ⊆ bigO l (map (map (pure HMul.hMul) s₁) s₂) := by
+  magic_tac
+  intro x
+  push _ ∈ _
+  rw [mem_bigO] at *
+  rintro ⟨p, hp, f, ⟨ι, instι, g, hg, hf⟩, rfl⟩
+  use ι, instι
+  use fun i x ↦ p x * g i x
+  constructor
+  · intro i
+    simp
+    use p, hp, g i, hg i
+  · simp_rw [norm_mul, ← Finset.mul_sum]
+    apply IsBigO.mul (IsBigO.norm_right (by rfl)) hf
+
+lemma bigO_mul {s₁ s₂ : Set (α → ℝ)} :
+    map (map (pure HMul.hMul) (bigO l s₁)) s₂ ⊆ bigO l (map (map (pure HMul.hMul) s₁) s₂) := by
+  have := mul_bigO (s₁ := s₂) (s₂ := s₁) (l := l)
+  magic_tac at *
+  convert this using 1 <;>
+  · rw [Set.iUnion₂_comm]
+    simp_rw [mul_comm]
+
+lemma bigO_add_bigO (s₁ s₂ : Set (α → E)) :
+    map (map (pure HAdd.hAdd) (bigO l s₁)) (bigO l s₂) = bigO l (s₁ ∪ s₂) := by
+  magic_tac
+  ext f
+  push _ ∈ _
+  simp_rw [mem_bigO]
+  constructor
+  · rintro ⟨f₁, ⟨ι₁, instι₁, g₁, hg₁, hf₁⟩, f₂, ⟨ι₂, instι₂, g₂, hg₂, hf₂⟩, rfl⟩
+    use ι₁ ⊕ ι₂, inferInstance
+    use Sum.rec g₁ g₂, by simp [*]
+    simp [← mul_div_right_comm]
+    convert IsBigO.add_add ?_ ?_ using 3
+    · symm; exact abs_of_nonneg (by positivity)
+    · symm; exact abs_of_nonneg (by positivity)
+    · exact hf₁
+    · exact hf₂
+  · rintro ⟨ι, instι, g, hg, hf⟩
+    rw [isBigO_iff'] at hf
+    obtain ⟨c, hc, hf⟩ := hf
+    simp (disch := positivity) only [norm_eq_abs, abs_of_nonneg] at hf
+    classical
+    let ι₁ : Type := {i // g i ∈ s₁}
+    let ι₂ : Type := {i // g i ∉ s₁}
+    let g₁ := fun j ↦ ∑ i : ι₁, ‖g i j‖
+    let g₂ := fun j ↦ ∑ i : ι₂, ‖g i j‖
+    have (j : α) : ∑ i : ι, ‖g i j‖ = g₁ j + g₂ j := by
+      symm
+      rw [Fintype.sum_subtype_add_sum_subtype (f := (‖g · j‖))]
+    simp_rw [this] at hf
+    use fun j ↦ if g₁ j < g₂ j then 0 else f j
+    constructor; swap
+    · use ι₁, inferInstance, fun i ↦ g i, fun ⟨i, hi⟩ ↦ by simpa using hi
+      rw [isBigO_iff]
+      use c * 2
+      filter_upwards [hf] with i hf
+      by_cases h : g₁ i < g₂ i <;> simp [h]
+      · positivity
+      · grw [hf, le_of_not_gt h, ← two_mul, mul_assoc, ← le_abs_self]
+    use fun j ↦ if g₁ j < g₂ j then f j else 0
+    constructor; swap
+    · use ι₂, inferInstance, fun i ↦ g i, fun ⟨i, hi⟩ ↦ by grind
+      rw [isBigO_iff]
+      use c * 2
+      filter_upwards [hf] with i hf
+      by_cases h : g₁ i < g₂ i <;> simp [h]
+      · grw [hf, h, ← two_mul, mul_assoc, ← le_abs_self]
+      · positivity
+    ext i
+    by_cases h : g₁ i < g₂ i <;> simp [h]
+
+
+-- O[l](f x) + O[l](f x) = O[l](f x)
+lemma bigO_add_bigO_set (f : Set (α → ℝ)) :
+    map (map (pure HAdd.hAdd) (bigO l f)) (bigO l f) = bigO l f := by
+  rw [bigO_add_bigO, Set.union_self]
+
+lemma bigO_add_bigO_eq_left (s₁ s₂ : Set (α → ℝ)) (h : bigO l s₂ ⊆ bigO l s₁) :
+    map (map (pure HAdd.hAdd) (bigO l s₁)) (bigO l s₂) = bigO l s₁ := by
+  apply subset_antisymm
+  · grw [h, bigO_add_bigO_set]
+  · grw [bigO_add_bigO, ← Set.subset_union_left]
+
+lemma bigO_add_bigO_eq_right (s₁ s₂ : Set (α → ℝ)) (h : bigO l s₁ ⊆ bigO l s₂) :
+    map (map (pure HAdd.hAdd) (bigO l s₁)) (bigO l s₂) = bigO l s₂ := by
+  apply subset_antisymm
+  · grw [h, bigO_add_bigO_set]
+  · grw [bigO_add_bigO, ← Set.subset_union_right]
+
+section RightSerial
+
+def RightSerial (r : α → β → Prop) (s₁ : Set α) (s₂ : Set β) : Prop :=
+  ∀ x₁ ∈ s₁, ∃ x₂ ∈ s₂, r x₁ x₂
+
+notation3 x " RS[" r "] " y => RightSerial r x y
+
+universe u v w
+
+instance (r : α → β → Prop) (s : β → γ → Prop) (t : α → γ → Prop) [Trans r s t] :
+  Trans (RightSerial r) (RightSerial s) (RightSerial t) where
+  trans hab hbc := by
+    simp only [RightSerial] at hab hbc ⊢
+    have := @Trans.trans (r := r) (s := s) (t := t) _ _ _ _
+    grind
+
+@[simp]
+lemma rightSerial_eq (a b : Set α) : (a RS[Eq] b) ↔ a ⊆ b := by
+  unfold RightSerial
+  grind
+
+@[simp, gcongr]
+lemma RightSerial.singleton_RS_singleton {r : α → α → Prop} (a : α) (b : α) :
+    ({a} RS[r] {b}) ↔ r a b := by
+  unfold RightSerial
+  simp
+
+/- Written by Claude -/
+@[gcongr]
+lemma RightSerial.iUnion_RS_iUnion {ι : Sort*} {r : α → β → Prop} {s : ι → Set α} {t : ι → Set β}
+    (h : ∀ i, s i RS[r] t i) : (⋃ i, s i) RS[r] (⋃ i, t i) := by
+  rintro x ⟨_, ⟨i, rfl⟩, hx⟩
+  obtain ⟨y, hy, hxy⟩ := h i x hx
+  exact ⟨y, Set.mem_iUnion.mpr ⟨i, hy⟩, hxy⟩
+
+class MapClass {α β γ : Type*} (r : (α → γ) → (α → γ) → Prop)
+    (r₁ : outParam ((α → β → γ) → (α → β → γ) → Prop)) (r₂ : outParam ((α → β) → (α → β) → Prop)) where
+  imp {f₁ f₂ f₁' f₂'} : r₁ f₁ f₁' → r₂ f₂ f₂' → r (fun x ↦ f₁ x (f₂ x)) (fun x ↦ f₁' x (f₂' x))
+
+@[gcongr]
+lemma map_rightSerial_map {s₁ s₁' : Set (α → β → γ)} {s₂ s₂' : Set (α → β)} {r r₁ r₂} [MapClass r r₁ r₂]
+    (h₁ : s₁ RS[r₁] s₁') (h₂ : s₂ RS[r₂] s₂') : map s₁ s₂ RS[r] map s₁' s₂' := by
+  rintro g ⟨f₁, hf₁, f₂, hf₂, rfl⟩
+  obtain ⟨f₁', hf₁', hff₁⟩ := h₁ f₁ hf₁
+  obtain ⟨f₂', hf₂', hff₂⟩ := h₂ f₂ hf₂
+  refine ⟨fun x ↦ f₁' x (f₂' x), ⟨f₁', hf₁', f₂', hf₂', rfl⟩, ?_⟩
+  exact MapClass.imp hff₁ hff₂
+
+instance {l : Filter α} : MapClass (α := α) (β := β) (γ := γ) (EventuallyEq l) (EventuallyEq l) (EventuallyEq l) where
+  imp h₁ h₂ := by
+    filter_upwards [h₁, h₂] with x hx₁ hx₂
+    simp [hx₁, hx₂]
+
+instance : MapClass (α := α) (β := β) (γ := γ) Eq Eq Eq where
+  imp h₁ h₂ := by simp [h₁, h₂]
+
+-- Should this be `gcongr` instead of `bigO_subset_bigO`?
+lemma bigO_subset_bigO' {s₁ s₂ : Set (α → ℝ)} (h : s₁ RS[(· =O[l] ·)] s₂) :
+    bigO l s₁ ⊆ bigO l s₂ := by
+  nth_rw 2 [← bigO_bigO]
+  gcongr
+  intro f hf
+  obtain ⟨g, hg, hf⟩ := h f hf
+  grw [hf, ← subset_bigO]
+  exact hg
+
+end RightSerial
+
+section Meta
 
 opaque dummyBigO [Norm E] (l : Filter α) (a : E) : E := a
 
@@ -333,41 +436,16 @@ partial def mappify (x : FVarId) (e : Expr) : MetaM Expr := do
   let e ← mkForallFVars #[.fvar x] e
   mkAppM ``singleton #[e]
 
-macro "magic_tac" loc:(Lean.Parser.Tactic.location)? : tactic => `(tactic|
-  simp only [map_eq, mem_pure, Set.mem_singleton_iff, Set.iUnion_iUnion_eq_left,
-    Set.mem_iUnion, exists_prop, Set.iUnion_exists, Set.biUnion_and'] $[$loc]?)
+end Meta
 
-lemma mul_bigO {f g : Set (ℝ → ℝ)} {l : Filter ℝ} :
-    map (map (pure HMul.hMul) f) (bigO l g) ⊆ bigO l (map (map (pure HMul.hMul) f) g) := by
-  magic_tac
-  intro x
-  push _ ∈ _
-  simp only [exists_prop, ↓existsAndEq, true_and, forall_exists_index, and_imp]
-  rintro p hp r q hq hr rfl
-  use p, hp, q, hq
-  exact IsBigO.mul (isBigO_refl p l) hr
-
-lemma bigO_mul {f g : Set (ℝ → ℝ)} {l : Filter ℝ} :
-    map (map (pure HMul.hMul) (bigO l f)) g ⊆ bigO l (map (map (pure HMul.hMul) f) g) := by
-  magic_tac
-  intro x
-  push _ ∈ _
-  simp only [exists_prop, ↓existsAndEq, true_and, forall_exists_index, and_imp]
-  rintro r p hp hr q hq rfl
-  use p, hp, q, hq
-  exact IsBigO.mul hr (isBigO_refl q l)
-attribute [refl] isBigO_refl
-
-@[gcongr]
-lemma bigO_subset_bigO {l : Filter ℝ} {s₁ s₂ : Set (ℝ → ℝ)} (h : ∀ f ∈ s₁, ∃ g ∈ s₂, f =O[l] g) :
-    bigO l s₁ ⊆ bigO l s₂ := by
-  intro f
-  simp only [mem_bigO, forall_exists_index, and_imp]
-  intro f' hf's hf'
-  specialize h f' hf's
-  grw [hf']
-  exact h
-
+/-
+  (n+1)^(e^(1/n))
+  = (n+1)^(1 + O(1/n)) := _
+  = exp ( (log n + O(1/n)) * (1 + O(1/n)) ) := _
+  = exp (log n + O(log n/n)) := _
+  = n * (1 + O(log n/n)) := _
+  = n + O(log n) := _
+-/
 theorem terry :
     map (map (pure HPow.hPow) (map (map (pure HAdd.hAdd) {fun x ↦ x}) (pure 1))) (map (pure Real.exp) (map (pure Inv.inv) {fun x ↦ x})) RS[EventuallyEq atTop]
       map (map (pure HAdd.hAdd) {fun x ↦ x}) (bigO Filter.atTop {Real.log}) := by
